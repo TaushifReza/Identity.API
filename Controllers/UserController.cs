f6f8071984e4cb1101f419d8cabcc78f8d0780db
MOHAMMAD Taushif Reza <112973122+TaushifReza@users.noreply.github.com>
2024-04-07 22:34:33 +0545

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,83 @@
+﻿using System.Net;
+using System.Net.Mail;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        public UserController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            this._userManager = userManager;
+            this._signInManager = signInManager;
+        }
+        [HttpPost("CreateUser")]
+        public async Task<IActionResult> CreateUser(string email, string password)
+        {
+            var user = new IdentityUser
+            {
+                Email = email,
+                UserName = email
+            };
+
+            var result = await this._userManager.CreateAsync(user, password);
+
+            if (result.Succeeded)
+            {
+                var confirmationToken = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                var message = new MailMessage("ecoshop420@gmail.com", user.Email, "Please Confirm your email",
+                    $"Please click on this link to confirm your email address: {confirmationToken}");
+                using (var emailClient = new SmtpClient("smtp.gmail.com", 587))
+                {
+                    emailClient.Credentials = new NetworkCredential("ecoshop420@gmail.com", "heaxhbpdjeszzhfy");
+                    await emailClient.SendMailAsync(message);
+                }
+                return Ok(confirmationToken ?? "User  Created");
+            }
+
+            return BadRequest(result.Errors);
+        }
+
+        [HttpPost("LogIn")]
+        public async Task<IActionResult> LogIn(string email, string password)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            if (user == null)
+            {
+                return BadRequest("Invalid email or password.");
+            }
+            var result  = await _signInManager.PasswordSignInAsync(user, password, false, false);
+            if (result.Succeeded)
+            {
+                return Ok("Login Success");
+            }
+            if (result.IsLockedOut)
+            {
+                return BadRequest("You are locked Out.");
+            }
+            return BadRequest("Login Failed");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ConfirmEmail(string userId, string token)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user != null)
+            {
+                var result = await _userManager.ConfirmEmailAsync(user, token);
+                if (result.Succeeded)
+                {
+                    return Ok("Email address is successfully confirmed, you can now try to login.");
+                }
+            }
+
+            return BadRequest("failed to validate");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+using Identity.API.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+    {
+        options.Password.RequiredLength = 6;
+        options.Password.RequireLowercase = true;
+        options.Password.RequireUppercase = true;
+
+        options.Lockout.MaxFailedAccessAttempts = 5;
+        options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
+
+        options.User.RequireUniqueEmail = true;
+        options.SignIn.RequireConfirmedEmail = false;
+    })
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+/*builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/acc/login";
+    options.AccessDeniedPath = "";
+});*/
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
